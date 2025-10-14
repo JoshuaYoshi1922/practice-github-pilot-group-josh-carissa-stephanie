4746480cfaadeac2a3916d033ce6f3d8dac7bf79
JoshuaYoshi1922 <joshua.yoshi1922@gmail.com>
2025-10-14 17:59:55 -0500

    phase 1

---
+++ practice-github-group/src/main/java/com/example/practice_github_group/PracticeGithubGroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_github_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubGroupApplication.class, args);
+	}
+
+}
---
+++ practice-github-group/src/test/java/com/example/practice_github_group/PracticeGithubGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_github_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
