8ddc4c8a4221e1f91cb7eda5c9efcf611639ae7e
stephveile <veile.steph@gmail.com>
2025-10-18 15:47:19 -0500

    Added unit tests

--- practice-github-group/src/test/java/com/example/practice_github_group/PracticeGithubGroupApplicationTests.java
+++ practice-github-group/src/test/java/com/example/practice_github_group/PracticeGithubGroupApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.practice_github_group;
 
+import com.example.practice_github_group.model.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PracticeGithubGroupApplicationTests {
@@ -9,5 +12,18 @@ class PracticeGithubGroupApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    public void testCreateStudent() {
+        Student student = new Student("Arthur Burrito");
+        assertEquals("Arthur Burrito", student.getName());
+    }
+
+    @Test
+    public void testUpdateStudent() {
+        Student student = new Student("Mae Mae");
+        student.setName("Cora Beanie");
+        assertEquals("Cora Beanie", student.getName());
+    }
 
 }
